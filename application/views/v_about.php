d23c35d78425bf74982af1d460407fc11c383207
nabilanorafi <nabillanorafii99@gmail.com>
2022-10-30 16:12:47 +0700

    push tugas pert 7

--- application/views/v_about.php
+++ application/views/v_about.php
@@ -2,13 +2,13 @@
      <h1><?php echo $judul ?></h1>
      <h4>Nama</h4>
      <ul type="disc">
-          <li>Nama Depan : Athariq Byan</li>
-          <li>Nama Belakang : Fadhlan Santoso</li>
+          <li>Nama Depan : Nabila</li>
+          <li>Nama Belakang : Norafi</li>
      </ul>
      <br>
      <h4>Alamat</h4>
      <ul type="none">
-          <li> Jalan Arjuna Gg. 14A No. 26 Slerok Tegal</li>
+          <li> Jl.pisang No.30A kraton kec.tegal barat</li>
      </ul>
 
      <h4>Tempat Lahir</h4>
@@ -18,7 +18,7 @@
 
      <h4>Olah Raga Favorit</h4>
      <ul type="square">
-          <li>Bulutangkis</li>
-          <li>Catur</li>
+          <li>Bolavoli</li>
+          <li>Berenang</li>
      </ul>
 </section>
